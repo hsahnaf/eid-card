02abcd34b0e425038a82ed26eb01592a88be6db5
AH_N_AF <130516863+hsahnaf@users.noreply.github.com>
2025-03-26 23:23:19 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const messageText = "আপনার জন্য রইলো ঈদের শুভেচ্ছা ও অফুরন্ত ভালোবাসা!";
+
+let index = 0;
+function typeMessage() {
+    if (index < messageText.length) {
+        document.getElementById("message").innerHTML += messageText.charAt(index);
+        index++;
+        setTimeout(typeMessage, 100);
+    }
+}
+
+window.onload = () => {
+    setTimeout(typeMessage, 1000);
+};
